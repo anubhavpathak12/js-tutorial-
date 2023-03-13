4f4106a202ffa6f6400f6cfed5a0d8baa051c429
Anubhav Pathak <anubhavpathak783@gmail.com>
2023-03-13 06:18:10 +0000

    learning dataType in js

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js code as a newer version
+
+//alert(3+3) // we are using nodejs, not a browser that's why it show error
+
+console.log(3 + 3)
+
+console.log("Anubhav")
+
+let name = "anubhav"
+let age = 19;
+let isLoggedIn = false
+
+// primitive datatype
+// number -> -(2^53-1) to(2^53-1) 
+// bigint 
+// string -> ""
+// boolean -> true/false
+// null-> standalone value(just a representation of empty value )
+
+let temp = null
+
+// undefined
+
+let state; //it is undefined
+
+// symbol -> used for uniqueness
+
+
+
+//object 
+
+console.log(typeof "Anubhav"); // used to know the type of value here 'string'
+
+console.log(typeof null) // here type is object
+console.log(typeof undefined) // type is undefined
