ef1f088023f6642330e738682d3ed82c8a43381f
Anubhav Pathak <anubhavpathak783@gmail.com>
2023-03-14 03:32:15 +0000

    update Learning Conversion & operation in js

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -24,3 +24,52 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ************** Operations ************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+*/
+
+let str1 ="hello"
+let str2 = " anubhav"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) //-> 12
+console.log(1 + "2") //-> 12
+console.log("1" + "2") //-> 12
+console.log("1" + 2 + 2) //-> 122
+console.log(1 + 2 + "2") // it gives 32
+
+console.log( ( (3 + 4) * 5) % 5) // 0
+
+let gameCounter = 103
+/* gameCounter++ */
+console.log(gameCounter++); 
+//-> first print 103 then increase by 1 so 103 -> 104
+
+// ++gameCounter
+console.log(++gameCounter); // it print 105 because ++gamecounter increase above 104 by 1 then print it 
+
+
+/*
+
+so if x = 4
+then prefix is '++x' it means that first increase x then print
+and
+the postfix is 'x++' it means that first print x then increase
+
+*/
