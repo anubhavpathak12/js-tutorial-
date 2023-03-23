1b1c1ee057cf9cfd88f3b81c0bd90976dace01c1
Anubhav Pathak <anubhavpathak783@gmail.com>
2023-03-23 16:32:29 +0000

    conditional Statement

---
+++ 01_basic/05_ConditionalStatement.js
@@ -0,0 +1,41 @@
+// if statement 
+
+let marks = 98;
+if(marks >= 90) {
+    console.log('A');
+}
+else if(marks >= 80) {
+    console.log('B');
+}
+else if(marks >= 70) {
+    console.log('C');
+}
+else if(marks >= 60) {
+    console.log('D');
+}
+else {
+    console.log('E');
+}
+
+// switch case
+let num = 2;
+switch (num) {
+    case 1 : console.log('A');
+    break;
+    case 2 : console.log('B');
+    break;
+    case 3 : console.log('C');
+    break;
+    case 4 : console.log('D');
+    break;
+
+}
+
+
+//while case
+var value = 1;
+while(value >= 0) {
+    console.log(value);
+    value = value + 1;
+
+}
