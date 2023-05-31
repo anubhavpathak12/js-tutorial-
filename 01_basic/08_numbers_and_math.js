310b8fb9eb73017dddedbcaf11609b8dbd3cd490
Anubhav Pathak <anubhavpathak783@gmail.com>
2023-05-31 03:08:58 +0000

    learning numbers and Math in JS

---
+++ 01_basic/08_numbers_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(typeof balance.toString());
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2)); 
+// toFixed is used to reducing the precision value upto 2 decimal, important use in ecommerce application
+
+const otherNumber = 123.89666
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4)); //4
+console.log(Math.round(5.8)) //6
+console.log(Math.floor(5.2)) //5
+console.log(Math.ceil(5.2)) //6
+console.log(Math.pow(10, 2)) //100
+console.log(Math.sqrt(25)); //5
+
+console.log(Math.min(4, 3, 5, 6, 8, 22)); //3
+console.log(Math.max(4, 3, 5, 6, 8, 22)); //22
