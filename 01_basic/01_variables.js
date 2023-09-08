77f98b0683c88f6af705fdc763bc40cc73e3587c
Anubhav Pathak <anubhavpathak783@gmail.com>
2023-09-08 04:32:33 +0000

    addings new comments

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,17 +1,17 @@
 const accountId = 144553
 let accountEmail = "anubhav@google.com"
 var accountPassward = "12345"
-accountCity = "Delhi"   // this is not good practice
+accountCity = "Delhi"   // this is not good practice but remember this is also one way to declare variable in js
 
 let accountState;
 //if we declare the variable without giving value, then js takes its value to be 'undefined' 
 
 
-// accountId = 2 // not allowed becz we declare with const
+// accountId = 2 // not allowed becz this is declare with const keyword already before that's why we can't change it after declaration 
 
-accountEmail = "pathak@anubhav.com"
+accountEmail = "pathakanubhav@google.com"
 accountPassward = "213567"
-accountCity = "Bihar"
+accountCity = "Silicon valley"
 
 console.log(accountId);
 
