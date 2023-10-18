47fb2356ca2edf87fa03e459b5074a7755b46014
Anubhav Pathak <anubhavpathak783@gmail.com>
2023-10-18 11:25:29 +0000

    object Class 2

---
+++ 02_basic/04_Objects.js
@@ -0,0 +1,62 @@
+// Singleton Object
+
+const myUser = new Object();
+
+myUser.id = "123abc";
+myUser.name = "Sumit";
+myUser.isLoggedIn = false;
+
+// console.log(myUser);
+
+const dailyUser = {
+    email: "hello@gmail.com",
+    fullname: {
+        userNickName: {
+            firstName: "Sumit",
+            lastName: "Pathak",
+        }
+    },
+}
+
+// console.log(dailyUser);
+
+// console.log(dailyUser["email"])
+
+// console.log(dailyUser.fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "e", 4: "f"}
+
+const obj3 = {...obj1, ...obj2}; // spread operator
+
+console.log(obj3);
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+// console.log(obj1);
+// console.log(obj1 === obj3); // false
+
+
+// jab database se object aayega tho arrays ke form mein aayega
+const users = [
+    {
+        id: 1,
+        email: "hello@gamil.com"
+    },
+    {
+        rollno: 1,
+        name: "anubhav pathak"
+    }
+]
+
+console.log(users[1]);
+
+console.log();
+
+console.log(myUser);
+console.log(Object.keys(myUser));
+console.log(Object.values(myUser));
+console.log(Object.entries(myUser));
+
+console.log(myUser.hasOwnProperty('isLoggedIn'));
