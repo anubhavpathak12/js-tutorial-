6e99e661793d3a416d12fe350fcada253e530d7a
Anubhav Pathak <anubhavpathak783@gmail.com>
2023-10-20 18:23:45 +0000

    learning about conditionals

---
+++ 04_Control_flow.js/one.js
@@ -0,0 +1,20 @@
+// if
+
+if(2 !== "2") {
+    console.log("executed"); // ab chalega
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+//Nullish Coalescing Operator (??) : null , undefined
+
+let val1;
+val1 = 5 ?? 10;
+
+val1 = null ?? 15 
+
+val1 = undefined ?? 15
+
+// console.log(val1);
+
